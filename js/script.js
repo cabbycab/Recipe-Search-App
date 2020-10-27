c3a4205e9943dcb2e2c42597f07e0e9bd1cc3b97
Danielle Koski <dkoski4@yahoo.com>
2020-10-27 11:28:47 -0700

    added functionin js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // let recipe
 
+let recipeData;
+
 function handleGetData(event){
     event.preventDefault();
     
@@ -7,20 +9,25 @@ function handleGetData(event){
     $.ajax({url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?rapidapi-key=fed654fad0msh1de46f7bf7bac2ap1221e6jsn663f78c78a48&query=?=${searchText}`})
       .then(
         (data) => {
-          console.log(data);
-          $("#title").text(data.title)
-          $("#ready").text(data.readyInMinutes)
-          $("#servings").text(data.servings)
-          $("#url").text(data.sourceUrl)
-          $("#image").attr("src", data.image)
+          recipeData = data;
+          renderRecipe()
         },
         (error) => {
           console.log("bad request: ", error)
-        }
-      )
-  }
+        });
+  };
+
   $('form').on("submit", handleGetData)
 
-//   function renderRecipe(recipes) {
-
-//   }
+  function renderRecipe(recipes) {
+    let recipeHtml = recipeData.results.map(function(recipe) {
+      return `
+        <img id="image" src="https://spoonacular.com/recipeImages/${recipe.image}" alt="${recipe.title}">
+        <h3>${recipe.title}</h3>
+        <p>Ready in: ${recipe.readyInMinutes} minutes</p>
+        <p>Servings: ${recipe.servings}</p>
+        <a href="${recipe.sourceUrl}">Click Here for Recipe</a>
+      `
+    });
+    $("main").html(recipeHtml)
+  };
