ebe0cc13b232fbd971cf64dedf21efadb25a1b9b
Danielle Koski <dkoski4@yahoo.com>
2021-01-28 17:33:17 -0800

    made mobile friendly

--- js/script.js
+++ js/script.js
@@ -2,30 +2,30 @@ let recipeData;
 
 function handleGetData(event) {
   event.preventDefault();
-  const searchText = $("#search").val()
+  const searchText = $("#search").val();
   $.ajax({
-      url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?rapidapi-key=fed654fad0msh1de46f7bf7bac2ap1221e6jsn663f78c78a48&query=?=${searchText}`
-    })
-    .then(
-      (data) => {
-        recipeData = data;
-        renderRecipe()
-      },
-      (error) => {
-        console.log("bad request: ", error)
-      });
-};
+    url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?rapidapi-key=fed654fad0msh1de46f7bf7bac2ap1221e6jsn663f78c78a48&query=?=${searchText}`,
+  }).then(
+    (data) => {
+      recipeData = data;
+      renderRecipe();
+    },
+    (error) => {
+      console.log("bad request: ", error);
+    }
+  );
+}
 
-$('form').on("submit", handleGetData)
+$("form").on("submit", handleGetData);
 
 function renderRecipe(recipes) {
-  let recipeHtml = recipeData.results.map(function(recipe) {
+  let recipeHtml = recipeData.results.map(function (recipe) {
     return `
-        <div><p><img id="image" src="https://spoonacular.com/recipeImages/${recipe.image}" alt="${recipe.title}"><br><b>${recipe.title}</b> <br>
+        <div id="content"><p><img id="image" src="https://spoonacular.com/recipeImages/${recipe.image}" alt="${recipe.title}"><br><b>${recipe.title}</b> <br>
         Ready in: ${recipe.readyInMinutes} minutes<br>
         Serves: ${recipe.servings}<br>
         <a href="${recipe.sourceUrl}">Click Here for Recipe</a></p></div>
-      `
+      `;
   });
-  $("main").html(recipeHtml)
-};
+  $("main").html(recipeHtml);
+}
